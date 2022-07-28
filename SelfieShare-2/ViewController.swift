27bbd4a0cec08203606e80470ba1dda3f454f023
Nikita <69157423+nktshklv-dev@users.noreply.github.com>
2022-07-28 23:04:27 +0300

    Initial Commit

---
+++ SelfieShare-2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SelfieShare-2
+//
+//  Created by Nikita  on 7/28/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
